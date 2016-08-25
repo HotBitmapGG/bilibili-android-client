a4a3a2d08a992c161bce42523a484b720e8cebb1
hotshotGG <100332338@qq.com>
2016-08-25 23:24:48 +0800

    update bangumi ui

---
+++ app/src/main/java/com/hotbitmapgg/ohmybilibili/adapter/WeekDayBangumiAdapter.java
@@ -0,0 +1,162 @@
+package com.hotbitmapgg.ohmybilibili.adapter;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.hotbitmapgg.ohmybilibili.R;
+import com.hotbitmapgg.ohmybilibili.entity.bangumi.WeekDayBangumi;
+import com.hotbitmapgg.ohmybilibili.module.bangumi.SpecialDetailsActivity;
+import com.hotbitmapgg.ohmybilibili.widget.sectioned.StatelessSection;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+/**
+ * Created by hcc on 16/8/25 20:52
+ * 100332338@qq.com
+ * <p/>
+ * 二三次元番剧专题Adapter
+ */
+public class WeekDayBangumiAdapter extends StatelessSection
+{
+
+
+    private Context mContext;
+
+    private int iconRes;
+
+    private String title;
+
+    private List<WeekDayBangumi> weekDayBangumis;
+
+
+    public WeekDayBangumiAdapter(Context context, List<WeekDayBangumi> weekDayBangumis, int iconRes, String title)
+    {
+
+        super(R.layout.layout_weekday_bangumi_head, R.layout.layout_weekday_bangumi_boby);
+        this.weekDayBangumis = weekDayBangumis;
+        this.mContext = context;
+        this.iconRes = iconRes;
+        this.title = title;
+    }
+
+    @Override
+    public int getContentItemsTotal()
+    {
+
+        return weekDayBangumis.size();
+    }
+
+    @Override
+    public RecyclerView.ViewHolder getItemViewHolder(View view)
+    {
+
+        return new ItemViewHolder(view);
+    }
+
+    @Override
+    public void onBindItemViewHolder(RecyclerView.ViewHolder holder, int position)
+    {
+
+        ItemViewHolder itemViewHolder = (ItemViewHolder) holder;
+        final WeekDayBangumi weekDayBangumi = weekDayBangumis.get(position);
+        itemViewHolder.mTitle.setText(weekDayBangumi.title);
+        itemViewHolder.mWeekDayText.setText(String.format(mContext.getString(R.string.weekday_update),
+                mContext.getResources().getStringArray(R.array.weekdays)[weekDayBangumi.weekday])
+        );
+
+        if (!TextUtils.isEmpty(weekDayBangumi.cover))
+        {
+            Picasso.with(mContext)
+                    .load(Uri.parse(weekDayBangumi.cover))
+                    .placeholder(R.drawable.bili_default_image_tv)
+                    .error(R.drawable.bili_default_image_tv)
+                    .into(itemViewHolder.mImageView);
+        } else
+        {
+            itemViewHolder.mImageView.setImageResource(R.drawable.bili_default_image_tv);
+        }
+
+        itemViewHolder.mCardView.setOnClickListener(new View.OnClickListener()
+        {
+
+            @Override
+            public void onClick(View v)
+            {
+
+                SpecialDetailsActivity.launch(
+                        (Activity) mContext,
+                        weekDayBangumi.spid,
+                        weekDayBangumi.title,
+                        weekDayBangumi.season_id);
+            }
+        });
+    }
+
+    @Override
+    public RecyclerView.ViewHolder getHeaderViewHolder(View view)
+    {
+
+        return new HeaderViewHolder(view);
+    }
+
+    @Override
+    public void onBindHeaderViewHolder(RecyclerView.ViewHolder holder)
+    {
+
+        HeaderViewHolder headerViewHolder = (HeaderViewHolder) holder;
+        Drawable drawable = mContext.getDrawable(iconRes);
+        if (drawable != null)
+            drawable.setBounds(0, 0, drawable.getMinimumWidth(), drawable.getMinimumHeight());
+        headerViewHolder.mWeekDay.setCompoundDrawables(drawable, null, null, null);
+        headerViewHolder.mWeekDay.setText(title);
+    }
+
+    class HeaderViewHolder extends RecyclerView.ViewHolder
+    {
+
+        @Bind(R.id.item_weekday)
+        TextView mWeekDay;
+
+        public HeaderViewHolder(View view)
+        {
+
+            super(view);
+            ButterKnife.bind(this, view);
+        }
+    }
+
+    class ItemViewHolder extends RecyclerView.ViewHolder
+    {
+
+        @Bind(R.id.item_img)
+        ImageView mImageView;
+
+        @Bind(R.id.item_title)
+        TextView mTitle;
+
+        @Bind(R.id.item_weekday)
+        TextView mWeekDayText;
+
+        @Bind(R.id.card_view)
+        CardView mCardView;
+
+        public ItemViewHolder(View view)
+        {
+
+            super(view);
+            ButterKnife.bind(this, view);
+        }
+    }
+}
--- app/src/main/java/com/hotbitmapgg/ohmybilibili/module/bangumi/WeekDayBangumiActivity.java
+++ app/src/main/java/com/hotbitmapgg/ohmybilibili/module/bangumi/WeekDayBangumiActivity.java
@@ -4,19 +4,33 @@ package com.hotbitmapgg.ohmybilibili.module.bangumi;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentStatePagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
 
-import com.flyco.tablayout.SlidingTabLayout;
 import com.hotbitmapgg.ohmybilibili.R;
+import com.hotbitmapgg.ohmybilibili.adapter.WeekDayBangumiAdapter;
 import com.hotbitmapgg.ohmybilibili.base.RxAppCompatBaseActivity;
+import com.hotbitmapgg.ohmybilibili.config.Secret;
+import com.hotbitmapgg.ohmybilibili.entity.bangumi.WeekDayBangumi;
+import com.hotbitmapgg.ohmybilibili.entity.bangumi.WeekDayBangumiResult;
+import com.hotbitmapgg.ohmybilibili.network.RetrofitHelper;
+import com.hotbitmapgg.ohmybilibili.utils.LogUtil;
+import com.hotbitmapgg.ohmybilibili.widget.CircleProgressView;
+import com.hotbitmapgg.ohmybilibili.widget.sectioned.SectionedRecyclerViewAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.Bind;
+import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+import rx.functions.Func1;
+import rx.schedulers.Schedulers;
 
 /**
  * Created by hcc on 16/8/4 14:12
@@ -27,22 +41,34 @@ import butterknife.Bind;
 public class WeekDayBangumiActivity extends RxAppCompatBaseActivity
 {
 
-    @Bind(R.id.view_pager)
-    ViewPager mViewPager;
-
-    @Bind(R.id.sliding_tabs)
-    SlidingTabLayout mTabLayout;
-
     @Bind(R.id.toolbar)
     Toolbar mToolbar;
+
+    @Bind(R.id.recycle)
+    RecyclerView mRecyclerView;
+
+    @Bind(R.id.circle_progress)
+    CircleProgressView mCircleProgressView;
+
+    private List<WeekDayBangumi> mOneWeekDayBangumis = new ArrayList<>();
+
+    private List<WeekDayBangumi> mTwoWeekDayBangumis = new ArrayList<>();
+
+    private List<WeekDayBangumi> mThreeWeekDayBangumis = new ArrayList<>();
+
+    private List<WeekDayBangumi> mFourWeekDayBangumis = new ArrayList<>();
+
+    private List<WeekDayBangumi> mFiveWeekDayBangumis = new ArrayList<>();
+
+    private List<WeekDayBangumi> mSixWeekDayBangumis = new ArrayList<>();
+
+    private List<WeekDayBangumi> mSevenWeekDayBangumis = new ArrayList<>();
+
+    private SectionedRecyclerViewAdapter mSectionedAdapter;
 
     private static final String EXTRA_TITLE = "extra_title";
 
     private static final String EXTRA_TYPE = "extra_type";
-
-    private BangumiFragmentAdapter mAdapter;
-
-    private String[] titles = new String[]{"一", "二", "三", "四", "五", "六", "日"};
 
     private int[] wids = new int[]{1, 2, 3, 4, 5, 6, 0};
 
@@ -50,11 +76,32 @@ public class WeekDayBangumiActivity extends RxAppCompatBaseActivity
 
     private int type;
 
+    private int[] weekDayIcons = new int[]{
+            R.drawable.bangumi_timeline_weekday_1,
+            R.drawable.bangumi_timeline_weekday_2,
+            R.drawable.bangumi_timeline_weekday_3,
+            R.drawable.bangumi_timeline_weekday_4,
+            R.drawable.bangumi_timeline_weekday_5,
+            R.drawable.bangumi_timeline_weekday_6,
+            R.drawable.bangumi_timeline_weekday_7
+    };
+
+    private String[] weekDayTitles = new String[]{
+            "周一",
+            "周二",
+            "周三",
+            "周四",
+            "周五",
+            "周六",
+            "周日"
+    };
+
+
     @Override
     public int getLayoutId()
     {
 
-        return R.layout.activity_bangumi_timeline;
+        return R.layout.activity_weekday_bangumi;
     }
 
     @Override
@@ -65,10 +112,200 @@ public class WeekDayBangumiActivity extends RxAppCompatBaseActivity
         title = intent.getStringExtra(EXTRA_TITLE);
         type = intent.getIntExtra(EXTRA_TYPE, 0);
 
-        mAdapter = new BangumiFragmentAdapter(getSupportFragmentManager());
-        mViewPager.setAdapter(mAdapter);
-        mTabLayout.setViewPager(mViewPager);
+        mSectionedAdapter = new SectionedRecyclerViewAdapter();
+        showProgressBar();
+        loadWeekDayBangumis();
     }
+
+    private void loadWeekDayBangumis()
+    {
+
+        RetrofitHelper.getWeekDayBangumiApi()
+                .getWeekDayBangumi(type, wids[0], Secret.APP_KEY,
+                        Long.toString(System.currentTimeMillis() / 1000))
+                .compose(this.<WeekDayBangumiResult> bindToLifecycle())
+                .flatMap(new Func1<WeekDayBangumiResult,Observable<WeekDayBangumiResult>>()
+                {
+
+                    @Override
+                    public Observable<WeekDayBangumiResult> call(WeekDayBangumiResult weekDayBangumiResult)
+                    {
+
+                        mOneWeekDayBangumis.addAll(weekDayBangumiResult.list);
+                        mSectionedAdapter.addSection(new WeekDayBangumiAdapter(
+                                WeekDayBangumiActivity.this,
+                                mOneWeekDayBangumis,
+                                weekDayIcons[0],
+                                weekDayTitles[0]));
+
+                        return RetrofitHelper.getWeekDayBangumiApi()
+                                .getWeekDayBangumi(type, wids[1], Secret.APP_KEY,
+                                        Long.toString(System.currentTimeMillis() / 1000));
+                    }
+                })
+                .flatMap(new Func1<WeekDayBangumiResult,Observable<WeekDayBangumiResult>>()
+                {
+
+                    @Override
+                    public Observable<WeekDayBangumiResult> call(WeekDayBangumiResult weekDayBangumiResult)
+                    {
+
+                        mTwoWeekDayBangumis.addAll(weekDayBangumiResult.list);
+                        mSectionedAdapter.addSection(new WeekDayBangumiAdapter(
+                                WeekDayBangumiActivity.this,
+                                mTwoWeekDayBangumis,
+                                weekDayIcons[1],
+                                weekDayTitles[1]));
+
+                        return RetrofitHelper.getWeekDayBangumiApi()
+                                .getWeekDayBangumi(type, wids[2], Secret.APP_KEY,
+                                        Long.toString(System.currentTimeMillis() / 1000));
+                    }
+                })
+                .flatMap(new Func1<WeekDayBangumiResult,Observable<WeekDayBangumiResult>>()
+                {
+
+                    @Override
+                    public Observable<WeekDayBangumiResult> call(WeekDayBangumiResult weekDayBangumiResult)
+                    {
+
+                        mThreeWeekDayBangumis.addAll(weekDayBangumiResult.list);
+                        mSectionedAdapter.addSection(new WeekDayBangumiAdapter(
+                                WeekDayBangumiActivity.this,
+                                mThreeWeekDayBangumis,
+                                weekDayIcons[2],
+                                weekDayTitles[2]));
+
+                        return RetrofitHelper.getWeekDayBangumiApi()
+                                .getWeekDayBangumi(type, wids[3], Secret.APP_KEY,
+                                        Long.toString(System.currentTimeMillis() / 1000));
+                    }
+                })
+                .flatMap(new Func1<WeekDayBangumiResult,Observable<WeekDayBangumiResult>>()
+                {
+
+                    @Override
+                    public Observable<WeekDayBangumiResult> call(WeekDayBangumiResult weekDayBangumiResult)
+                    {
+
+                        mFourWeekDayBangumis.addAll(weekDayBangumiResult.list);
+                        mSectionedAdapter.addSection(new WeekDayBangumiAdapter(
+                                WeekDayBangumiActivity.this,
+                                mFourWeekDayBangumis,
+                                weekDayIcons[3],
+                                weekDayTitles[3]));
+
+                        return RetrofitHelper.getWeekDayBangumiApi()
+                                .getWeekDayBangumi(type, wids[4], Secret.APP_KEY,
+                                        Long.toString(System.currentTimeMillis() / 1000));
+                    }
+                })
+                .flatMap(new Func1<WeekDayBangumiResult,Observable<WeekDayBangumiResult>>()
+                {
+
+                    @Override
+                    public Observable<WeekDayBangumiResult> call(WeekDayBangumiResult weekDayBangumiResult)
+                    {
+
+                        mFiveWeekDayBangumis.addAll(weekDayBangumiResult.list);
+                        mSectionedAdapter.addSection(new WeekDayBangumiAdapter(
+                                WeekDayBangumiActivity.this,
+                                mFiveWeekDayBangumis,
+                                weekDayIcons[4],
+                                weekDayTitles[4]));
+
+                        return RetrofitHelper.getWeekDayBangumiApi()
+                                .getWeekDayBangumi(type, wids[5], Secret.APP_KEY,
+                                        Long.toString(System.currentTimeMillis() / 1000));
+                    }
+                })
+                .flatMap(new Func1<WeekDayBangumiResult,Observable<WeekDayBangumiResult>>()
+                {
+
+                    @Override
+                    public Observable<WeekDayBangumiResult> call(WeekDayBangumiResult weekDayBangumiResult)
+                    {
+
+                        mSixWeekDayBangumis.addAll(weekDayBangumiResult.list);
+                        mSectionedAdapter.addSection(new WeekDayBangumiAdapter(
+                                WeekDayBangumiActivity.this,
+                                mSixWeekDayBangumis,
+                                weekDayIcons[5],
+                                weekDayTitles[5]));
+
+                        return RetrofitHelper.getWeekDayBangumiApi()
+                                .getWeekDayBangumi(type, wids[6], Secret.APP_KEY,
+                                        Long.toString(System.currentTimeMillis() / 1000));
+                    }
+                })
+                .flatMap(new Func1<WeekDayBangumiResult,Observable<String>>()
+                {
+
+                    @Override
+                    public Observable<String> call(WeekDayBangumiResult weekDayBangumiResult)
+                    {
+
+                        mSevenWeekDayBangumis.addAll(weekDayBangumiResult.list);
+                        mSectionedAdapter.addSection(new WeekDayBangumiAdapter(
+                                WeekDayBangumiActivity.this,
+                                mSevenWeekDayBangumis,
+                                weekDayIcons[6],
+                                weekDayTitles[6]));
+
+                        return Observable.just("success");
+                    }
+                })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Action1<String>()
+                {
+
+                    @Override
+                    public void call(String s)
+                    {
+
+                        finishGetTask();
+                    }
+                }, new Action1<Throwable>()
+                {
+
+                    @Override
+                    public void call(Throwable throwable)
+                    {
+
+                        LogUtil.all("番剧放送表加载失败" + throwable.getMessage());
+                        hideProgressBar();
+                    }
+                });
+    }
+
+
+    private void finishGetTask()
+    {
+
+        GridLayoutManager mLayoutManager = new GridLayoutManager(WeekDayBangumiActivity.this, 3);
+        mLayoutManager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup()
+        {
+
+            @Override
+            public int getSpanSize(int position)
+            {
+
+                switch (mSectionedAdapter.getSectionItemViewType(position))
+                {
+                    case SectionedRecyclerViewAdapter.VIEW_TYPE_HEADER:
+                        return 3;
+                    default:
+                        return 1;
+                }
+            }
+        });
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mSectionedAdapter);
+
+        hideProgressBar();
+    }
+
 
     @Override
     public void initToolBar()
@@ -103,42 +340,18 @@ public class WeekDayBangumiActivity extends RxAppCompatBaseActivity
         activity.startActivity(mIntent);
     }
 
-    private class BangumiFragmentAdapter extends FragmentStatePagerAdapter
+
+    public void showProgressBar()
     {
 
+        mCircleProgressView.setVisibility(View.VISIBLE);
+        mCircleProgressView.spin();
+    }
 
-        public BangumiFragmentAdapter(FragmentManager fm)
-        {
+    public void hideProgressBar()
+    {
 
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position)
-        {
-
-            return WeekDayBangumiFragment.newInstance(wids[position], type);
-        }
-
-        @Override
-        public int getCount()
-        {
-
-            return titles.length;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position)
-        {
-
-            return titles[position];
-        }
-
-        @Override
-        public int getItemPosition(Object object)
-        {
-
-            return POSITION_NONE;
-        }
+        mCircleProgressView.setVisibility(View.GONE);
+        mCircleProgressView.stopSpinning();
     }
 }
