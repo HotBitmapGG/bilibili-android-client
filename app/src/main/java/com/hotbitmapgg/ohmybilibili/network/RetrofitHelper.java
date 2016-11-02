10bff8b3b7c343f0cc9a5c5533256ad2adc5a259
hotshotGG <100332338@qq.com>
2016-11-02 11:17:58 +0800

    Update Okhttp Cache

--- app/src/main/java/com/hotbitmapgg/ohmybilibili/network/RetrofitHelper.java
+++ app/src/main/java/com/hotbitmapgg/ohmybilibili/network/RetrofitHelper.java
@@ -40,12 +40,14 @@ import com.hotbitmapgg.ohmybilibili.network.api.UserPlayGameService;
 import com.hotbitmapgg.ohmybilibili.network.api.VideoCommentService;
 import com.hotbitmapgg.ohmybilibili.network.api.VideoDetailsService;
 import com.hotbitmapgg.ohmybilibili.network.api.VipGameService;
+import com.hotbitmapgg.ohmybilibili.utils.CommonUtil;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 import okhttp3.Cache;
+import okhttp3.CacheControl;
 import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -806,11 +808,7 @@ public class RetrofitHelper
 
 
     /**
-     * 初始化OKHttpClient
-     * 设置缓存
-     * 设置超时时间
-     * 设置打印日志
-     * 设置UA拦截器
+     * 初始化OKHttpClient,设置缓存,设置超时时间,设置打印日志,设置UA拦截器
      */
     private static void initOkHttpClient()
     {
@@ -825,7 +823,7 @@ public class RetrofitHelper
                 {
                     //设置Http缓存
                     Cache cache = new Cache(new File(BilibiliApp.getInstance()
-                            .getCacheDir(), "HttpCache"), 1024 * 1024 * 100);
+                            .getCacheDir(), "HttpCache"), 1024 * 1024 * 10);
 
                     mOkHttpClient = new OkHttpClient.Builder()
                             .cache(cache)
@@ -865,8 +863,7 @@ public class RetrofitHelper
 
 
     /**
-     * 为okhttp添加缓存，缓存时间为1小时，
-     * 这里是考虑到服务器不支持缓存时，从而让okhttp支持缓存
+     * 为okhttp添加缓存，这里是考虑到服务器不支持缓存时，从而让okhttp支持缓存
      */
     private static class CacheInterceptor implements Interceptor
     {
@@ -875,12 +872,39 @@ public class RetrofitHelper
         public Response intercept(Chain chain) throws IOException
         {
 
+            // 有网络时 设置缓存超时时间1个小时
+            int maxAge = 60 * 60;
+            // 无网络时，设置超时为1天
+            int maxStale = 60 * 60 * 24;
             Request request = chain.request();
-            return chain.proceed(request).newBuilder()
-                    .removeHeader("Pragma")
-                    .removeHeader("Cache-Control")
-                    .header("Cache-Control", "max-age=" + 3600)
-                    .build();
+            if (CommonUtil.isNetworkAvailable(BilibiliApp.getInstance()))
+            {
+                //有网络时只从网络获取
+                request = request.newBuilder()
+                        .cacheControl(CacheControl.FORCE_NETWORK)
+                        .build();
+            } else
+            {
+                //无网络时只从缓存中读取
+                request = request.newBuilder()
+                        .cacheControl(CacheControl.FORCE_CACHE)
+                        .build();
+            }
+            Response response = chain.proceed(request);
+            if (CommonUtil.isNetworkAvailable(BilibiliApp.getInstance()))
+            {
+                response = response.newBuilder()
+                        .removeHeader("Pragma")
+                        .header("Cache-Control", "public, max-age=" + maxAge)
+                        .build();
+            } else
+            {
+                response = response.newBuilder()
+                        .removeHeader("Pragma")
+                        .header("Cache-Control", "public, only-if-cached, max-stale=" + maxStale)
+                        .build();
+            }
+            return response;
         }
     }
 }
