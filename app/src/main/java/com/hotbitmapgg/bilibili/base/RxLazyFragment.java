db75004b90672c58a76867d21e79cad905e723da
Hcc <hotbitmapgg@gmail.com>
2017-06-26 13:55:33 +0800

    update base class format

--- app/src/main/java/com/hotbitmapgg/bilibili/base/RxLazyFragment.java
+++ app/src/main/java/com/hotbitmapgg/bilibili/base/RxLazyFragment.java
@@ -1,9 +1,5 @@
 package com.hotbitmapgg.bilibili.base;
 
-import butterknife.ButterKnife;
-import butterknife.Unbinder;
-import com.trello.rxlifecycle.components.support.RxFragment;
-
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
@@ -13,6 +9,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.trello.rxlifecycle.components.support.RxFragment;
+
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
 /**
  * Created by hcc on 16/8/7 21:18
  * 100332338@qq.com
@@ -20,147 +21,156 @@ import android.view.ViewGroup;
  * Fragment基类
  */
 public abstract class RxLazyFragment extends RxFragment {
+    private View parentView;
+    private FragmentActivity activity;
+    // 标志位 标志已经初始化完成。
+    protected boolean isPrepared;
+    //标志位 fragment是否可见
+    protected boolean isVisible;
+    private Unbinder bind;
 
-  private View parentView;
+    public abstract
+    @LayoutRes
+    int getLayoutResId();
 
-  private FragmentActivity activity;
-
-  // 标志位 标志已经初始化完成。
-  protected boolean isPrepared;
-
-  //标志位 fragment是否可见
-  protected boolean isVisible;
-
-  private Unbinder bind;
-
-
-  public abstract
-  @LayoutRes
-  int getLayoutResId();
-
-
-  @Override
-  public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle state) {
-    parentView = inflater.inflate(getLayoutResId(), container, false);
-    activity = getSupportActivity();
-    return parentView;
-  }
-
-
-  @Override
-  public void onViewCreated(View view, Bundle savedInstanceState) {
-
-    super.onViewCreated(view, savedInstanceState);
-    bind = ButterKnife.bind(this, view);
-    finishCreateView(savedInstanceState);
-  }
-
-
-  public abstract void finishCreateView(Bundle state);
-
-
-  @Override
-  public void onResume() {
-
-    super.onResume();
-  }
-
-
-  @Override
-  public void onDestroyView() {
-
-    super.onDestroyView();
-    bind.unbind();
-  }
-
-
-  @Override
-  public void onAttach(Activity activity) {
-
-    super.onAttach(activity);
-    this.activity = (FragmentActivity) activity;
-  }
-
-
-  @Override
-  public void onDetach() {
-
-    super.onDetach();
-    this.activity = null;
-  }
-
-
-  public FragmentActivity getSupportActivity() {
-
-    return super.getActivity();
-  }
-
-
-  public android.app.ActionBar getSupportActionBar() {
-
-    return getSupportActivity().getActionBar();
-  }
-
-
-  public Context getApplicationContext() {
-
-    return this.activity == null
-           ? (getActivity() == null ? null :
-              getActivity().getApplicationContext())
-           : this.activity.getApplicationContext();
-  }
-
-
-  /**
-   * Fragment数据的懒加载
-   */
-  @Override
-  public void setUserVisibleHint(boolean isVisibleToUser) {
-
-    super.setUserVisibleHint(isVisibleToUser);
-    if (getUserVisibleHint()) {
-      isVisible = true;
-      onVisible();
-    } else {
-      isVisible = false;
-      onInvisible();
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle state) {
+        parentView = inflater.inflate(getLayoutResId(), container, false);
+        activity = getSupportActivity();
+        return parentView;
     }
-  }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        bind = ButterKnife.bind(this, view);
+        finishCreateView(savedInstanceState);
+    }
+
+    /**
+     * 初始化views
+     *
+     * @param state
+     */
+    public abstract void finishCreateView(Bundle state);
 
 
-  protected void onVisible() {
-
-    lazyLoad();
-  }
-
-
-  protected void lazyLoad() {}
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
 
 
-  protected void onInvisible() {}
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        bind.unbind();
+    }
 
 
-  protected void loadData() {}
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        this.activity = (FragmentActivity) activity;
+    }
 
 
-  protected void showProgressBar() {}
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        this.activity = null;
+    }
 
 
-  protected void hideProgressBar() {}
+    public FragmentActivity getSupportActivity() {
+        return super.getActivity();
+    }
 
 
-  protected void initRecyclerView() {}
+    public android.app.ActionBar getSupportActionBar() {
+        return getSupportActivity().getActionBar();
+    }
 
 
-  protected void initRefreshLayout() {}
+    public Context getApplicationContext() {
+        return this.activity == null ? (getActivity() == null ?
+                null : getActivity().getApplicationContext()) : this.activity.getApplicationContext();
+    }
 
 
-  protected void finishTask() {}
+    /**
+     * Fragment数据的懒加载
+     */
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+        if (getUserVisibleHint()) {
+            isVisible = true;
+            onVisible();
+        } else {
+            isVisible = false;
+            onInvisible();
+        }
+    }
+
+    /**
+     * fragment显示时才加载数据
+     */
+    protected void onVisible() {
+        lazyLoad();
+    }
+
+    /**
+     * fragment懒加载方法
+     */
+    protected void lazyLoad() {
+    }
+
+    /**
+     * fragment隐藏
+     */
+    protected void onInvisible() {
+    }
+
+    /**
+     * 加载数据
+     */
+    protected void loadData() {
+    }
+
+    /**
+     * 显示进度条
+     */
+    protected void showProgressBar() {
+    }
+
+    /**
+     * 隐藏进度条
+     */
+    protected void hideProgressBar() {
+    }
+
+    /**
+     * 初始化recyclerView
+     */
+    protected void initRecyclerView() {
+    }
+
+    /**
+     * 初始化refreshLayout
+     */
+    protected void initRefreshLayout() {
+    }
+
+    /**
+     * 设置数据显示
+     */
+    protected void finishTask() {
+    }
 
 
-  @SuppressWarnings("unchecked")
-  public <T extends View> T $(int id) {
-
-    return (T) parentView.findViewById(id);
-  }
+    @SuppressWarnings("unchecked")
+    public <T extends View> T $(int id) {
+        return (T) parentView.findViewById(id);
+    }
 }
